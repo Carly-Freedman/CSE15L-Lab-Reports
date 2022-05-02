43712ae1b262ae682b50a1c950e0f4ba667f03ad
Carly-Freedman <freedmcb0023@student.laccd.edu>
2022-05-02 12:29:40 -0700

    Working Test Case

--- SkillDemoPractice.java
+++ SkillDemoPractice.java
@@ -2,6 +2,9 @@
 
 public class SkillDemoPractice{
     public static String division(int n1, int n2){
+        if(n2 == 0){
+            return "undefined";
+        }
         return String.valueOf(n1/n2);
     }
     public static void main(String args[]){

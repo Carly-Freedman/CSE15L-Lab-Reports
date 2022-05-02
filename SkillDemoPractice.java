e4adda0548efa62e32592766ca665b158fe9626f
Carly-Freedman <freedmcb0023@student.laccd.edu>
2022-05-02 12:24:05 -0700

    Failing Test

---
+++ SkillDemoPractice.java
@@ -0,0 +1,10 @@
+
+
+public class SkillDemoPractice{
+    public static String division(int n1, int n2){
+        return String.valueOf(n1/n2);
+    }
+    public static void main(String args[]){
+        return;
+    }
+}
---
+++ SkillDemoPracticeTest.java
@@ -0,0 +1,12 @@
+
+//import SkillDemo.SkillDemoPractice;
+import static org.junit.Assert.*; 
+import org.junit.*;
+
+public class SkillDemoPracticeTest { 
+    @Test 
+    public void divisionTest() { 
+        assertEquals("undefined", SkillDemoPractice.division(5, 0)); 
+    }
+
+    }
